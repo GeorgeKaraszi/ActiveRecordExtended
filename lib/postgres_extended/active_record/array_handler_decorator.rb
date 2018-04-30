e82350bffc3986ae8c72c23f794caeb5726d53d6
George Karaszi <georgekaraszi@gmail.com>
2018-04-30 10:30:48 -0400

    (AR 5.0.x) Fix contains array field when joining another table

--- lib/postgres_extended/active_record/array_handler_decorator.rb
+++ lib/postgres_extended/active_record/array_handler_decorator.rb
@@ -6,18 +6,13 @@ require "active_record/relation/predicate_builder/array_handler"
 module PostgresExtended
   module ArrayHandlerDecorator
     def call(attribute, value)
-      column =
-        case attribute.try(:relation)
-        when Arel::Nodes::TableAlias, NilClass
-          nil
-        else
-          cache = ActiveRecord::Base.connection.schema_cache
-          if cache.data_source_exists?(attribute.relation.name)
-            cache.columns(attribute.relation.name).detect { |col| col.name.to_s == attribute.name.to_s }
-          end
-        end
+      cache = ActiveRecord::Base.connection.schema_cache
+      if cache.data_source_exists?(attribute.relation.name)
+        column = cache.columns(attribute.relation.name).detect { |col| col.name.to_s == attribute.name.to_s }
+        return attribute.eq(value) if column.try(:array)
+      end
 
-      column.try(:array) ? attribute.eq(value) : super
+      super(attribute, value)
     end
   end
 end
