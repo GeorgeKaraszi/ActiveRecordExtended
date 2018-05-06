b88bb5e4caac568655092a36538b324288c36dce
George Protacio-Karaszi <georgekaraszi@gmail.com>
2018-05-06 11:28:28 -0400

    Implement relationship handling

--- lib/active_record_extended/query_methods/any_of.rb
+++ lib/active_record_extended/query_methods/any_of.rb
@@ -5,12 +5,12 @@ module ActiveRecordExtended
     module AnyOf
       def any_of(*queries)
         queries = hash_map_queries(queries)
-        @scope.where(build_or_queries(queries))
+        build_query(queries) { |arel_query| @scope.where(arel_query) }
       end
 
       def none_of(*queries)
         queries = hash_map_queries(queries)
-        @scope.where.not(build_or_queries(queries))
+        build_query(queries) { |arel_query| @scope.where.not(arel_query) }
       end
 
       private
@@ -23,19 +23,28 @@ module ActiveRecordExtended
         end
       end
 
-      def build_quries(queries)
-        where_queries = queries(queries)
+      def build_query(queries)
+        query_map = construct_query_mappings(queries)
+        query = yield query_map[:arel_query]
+        query.joins(query_map[:joins].to_a)
+             .includes(query_map[:includes].to_a)
+             .references(query_map[:references].to_a)
       end
 
-      def queries(queries)
-       queries.map do |query|
-         query = generate_where_clause(query)
-         Arel::SelectManager
-         i = q.arel
-         con = q.arel.constraints
-         c = q.arel.constraints.reduce(:and)
-         q
-       end.reduce(:group_or)
+      def construct_query_mappings(queries) # rubocop:disable Metrics/AbcSize
+        { joins: Set.new, references: Set.new, includes: Set.new, arel_query: nil }.tap do |query_map|
+          query_map[:arel_query] = queries.map do |raw_query|
+            query = generate_where_clause(raw_query)
+            query_map[:joins]      << translate_reference(query.joins_values)      if query.joins_values.any?
+            query_map[:includes]   << translate_reference(query.includes_values)   if query.includes_values.any?
+            query_map[:references] << translate_reference(query.references_values) if query.references_values.any?
+            query.arel.constraints.reduce(:and)
+          end.reduce(:group_or)
+        end
+      end
+
+      def translate_reference(reference)
+        reference.map { |ref| ref.try(:to_sql) || ref }.compact
       end
 
       def generate_where_clause(query)
--- spec/query_methods/any_of_spec.rb
+++ spec/query_methods/any_of_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe "Active Record Either Methods" do
   let!(:one)       { Person.create!(personal_id: 1) }
   let!(:two)       { Person.create!(personal_id: 2) }
@@ -5,7 +7,7 @@ RSpec.describe "Active Record Either Methods" do
 
   describe "where.any_of/1" do
     it "Should return queries that match any of the outlined queries" do
-      query = Person.where.any_of({ personal_id: 1 }, { personal_id: 2})
+      query = Person.where.any_of({ personal_id: 1 }, { personal_id: 2 })
       expect(query).to include(one, two)
       expect(query).to_not include(three)
     end
@@ -17,14 +19,6 @@ RSpec.describe "Active Record Either Methods" do
 
       expect(query).to include(one, two)
       expect(query).to_not include(three)
-    end
-
-    it "Works with bound attributes" do
-      personal_one = Person.where("personal_id >= 1").limit(2)
-      query = Person.where.any_of(personal_one)
-
-      expect(query).to include(one)
-      expect(query).to_not include(two, three)
     end
 
     it "Should accept query strings" do
@@ -39,18 +33,38 @@ RSpec.describe "Active Record Either Methods" do
       expect(query).to_not include(one)
     end
 
-    it "Should accept joined where queries" do
-      tag_one   = Tag.create!(person_id: one.id)
-      tag_two   = Tag.create!(person_id: two.id)
-      tag_three = Tag.create!(person_id: three.id)
+    context "Relationship queries" do
+      let!(:tag_one)   { Tag.create!(person_id: one.id) }
+      let!(:tag_two)   { Tag.create!(person_id: two.id) }
+      let!(:tag_three) { Tag.create!(person_id: three.id) }
 
-      query = Tag.joins(:person).where.any_of(
+      it "Finds records that are queried from two or more has_many associations" do
+        person_one_tag = Tag.create!(person_id: one.id)
+        person_two_tag = Tag.create!(person_id: two.id)
+        query          = Tag.where.any_of(one.hm_tags, two.hm_tags)
+
+        expect(query).to include(tag_one, tag_two, person_one_tag, person_two_tag)
+        expect(query).to_not include(tag_three)
+      end
+
+      it "Finds records that are dynamically joined" do
+        person_one_tag = Tag.where(people: { id: one.id }).includes(:person).references(:person)
+        person_two_tag = Tag.where(people: { id: two.id }).joins(:person)
+        query          = Tag.where.any_of(person_one_tag, person_two_tag)
+
+        expect(query).to include(tag_one, tag_two)
+        expect(query).to_not include(tag_three)
+      end
+
+      it "Return matched records of a joined table on the parent level" do
+        query = Tag.joins(:person).where.any_of(
           { people: { personal_id: 1 } },
-          { people: { personal_id: 3 } }
-      )
+          { people: { personal_id: 3 } },
+        )
 
-      expect(query).to include(tag_one, tag_three)
-      expect(query).to_not include(tag_two)
+        expect(query).to include(tag_one, tag_three)
+        expect(query).to_not include(tag_two)
+      end
     end
   end
 end
