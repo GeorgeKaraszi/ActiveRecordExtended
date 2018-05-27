39aa865735fab235541cba1f898d1d43a9b6b194
George Protacio-Karaszi <georgekaraszi@gmail.com>
2018-05-27 12:54:48 -0700

    Add test coverage report

--- lib/active_record_extended/arel/nodes.rb
+++ lib/active_record_extended/arel/nodes.rb
@@ -5,64 +5,31 @@ require "arel/nodes/binary"
 module Arel
   module Nodes
     class Overlap < Arel::Nodes::Binary
-      def operator
-        :"&&"
-      end
     end
 
     class Contains < Arel::Nodes::Binary
-      def operator
-        :>>
-      end
     end
 
     class ContainsHStore < Arel::Nodes::Binary
-      def operator
-        :"@>"
-      end
     end
 
     class ContainsArray < Arel::Nodes::Binary
-      def operator
-        :"@>"
-      end
     end
 
     class ContainedInArray < Arel::Nodes::Binary
-      def operator
-        :"<@"
-      end
     end
 
     module Inet
       class ContainsEquals < Arel::Nodes::Binary
-        def operator
-          :">>="
-        end
       end
 
       class ContainedWithin < Arel::Nodes::Binary
-        def operator
-          :<<
-        end
       end
 
       class ContainedWithinEquals < Arel::Nodes::Binary
-        def operator
-          :"<<="
-        end
       end
 
       class ContainsOrContainedWithin < Arel::Nodes::Binary
-        def operator
-          :"&&"
-        end
-      end
-    end
-
-    class Node
-      def group_or(right)
-        Arel::Nodes::Or.new self, Arel::Nodes::Grouping.new(right)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
 require "active_record_extended"
 
 unless ENV["DATABASE_URL"]
