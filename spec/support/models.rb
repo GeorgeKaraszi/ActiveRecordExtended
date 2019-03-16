c805ec40fab1bfdd95b31e136a98494d35f09538
George Karaszi <georgekaraszi@gmail.com>
2019-03-16 14:16:15 -0700

    update model to include its related schema attributes

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -8,22 +8,41 @@ class Person < ApplicationRecord
   has_many :hm_tags, class_name: "Tag"
   has_one :profile_l, class_name: "ProfileL"
   has_one :profile_r, class_name: "ProfileR"
+  # attributes
+  # t.string   "tags",         array: true
+  # t.integer  "number",       default: 0
+  # t.integer  "personal_id"
+  # t.hstore   "data"
+  # t.jsonb    "jsonb_data"
+  # t.inet     "ip"
+  # t.cidr     "subnet"
+  #
 end
 
 class Tag < ApplicationRecord
   belongs_to :person
+  # attributes: tag_number
 end
 
 class ProfileL < ApplicationRecord
   belongs_to :person
   has_one :version, as: :versionable, class_name: "VersionControl"
+  # attributes
+  # t.integer :likes
+  #
 end
 
 class ProfileR < ApplicationRecord
   belongs_to :person
   has_one :version, as: :versionable, class_name: "VersionControl"
+  # attributes
+  # t.integer :dislikes
+  #
 end
 
 class VersionControl < ApplicationRecord
   belongs_to :versionable, polymorphic: true, optional: false
+  # attributes
+  # t.jsonb :source, default: {}, null: false
+  #
 end
