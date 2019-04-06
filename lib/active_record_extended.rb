11983363de0452053d033c7cdc66216be4143c8e
George Karaszi <georgekaraszi@gmail.com>
2019-04-06 12:00:42 -0700

    Extract ordering process operations into a module

--- lib/active_record_extended.rb
+++ lib/active_record_extended.rb
@@ -2,6 +2,7 @@
 
 require "active_record_extended/version"
 require "active_record_extended/utilities"
+require "active_record_extended/order_by_utilities"
 require "active_record_extended/active_record"
 require "active_record_extended/arel"
 
