5e29faa1337495d348adc9e6027b07c7acbf5458
George Karaszi <georgekaraszi@gmail.com>
2020-12-18 13:33:16 -0800

    Implement name function for SqlLiteral to allow Rails 6.1 to process CTE's

--- lib/active_record_extended/arel.rb
+++ lib/active_record_extended/arel.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "active_record_extended/arel/nodes"
+require "active_record_extended/arel/sql_literal"
 require "active_record_extended/arel/aggregate_function_name"
 require "active_record_extended/arel/predications"
 require "active_record_extended/arel/visitors/postgresql_decorator"
---
+++ lib/active_record_extended/arel/sql_literal.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "arel/nodes/sql_literal"
+
+# CTE alias fix for Rails 6.1
+module Arel
+  module Nodes
+    module SqlLiteralDecorator
+      def name
+        self
+      end
+    end
+  end
+end
+
+Arel::Nodes::SqlLiteral.prepend(Arel::Nodes::SqlLiteralDecorator)
