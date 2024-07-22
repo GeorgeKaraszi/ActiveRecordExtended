2e2cfd93e946db4796e798b8a493975d44082c16
Garrett Blehm <gblehm@fleetio.com>
2024-07-22 08:47:50 -0500

    Fixes Issue #91: SystemStackError when using .with (#105)
    Signed-off-by: Garrett Blehm <gblehm@fleetio.com>

--- lib/active_record_extended/query_methods/with_cte.rb
+++ lib/active_record_extended/query_methods/with_cte.rb
@@ -54,6 +54,8 @@ module ActiveRecordExtended
             # Ensure we follow FIFO pattern.
             # If the parent has similar CTE alias keys, we want to favor the parent's expressions over its children's.
             if expression.is_a?(ActiveRecord::Relation) && expression.with_values?
+              expression.cte = expression.cte.dup if expression.cte
+
               # Add child's materialized keys to the parent
               @materialized_keys += expression.cte.materialized_keys
               @not_materialized_keys += expression.cte.not_materialized_keys
